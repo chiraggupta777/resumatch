c2dc6fcce7473015ef8996bd082a7a3f90213acc
Chirag Gupta <guptachirag7777@gmail.com>
2026-07-05 11:54:06 +0530

    Update CORS for deployment

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,7 @@ const authRoutes = require('./routes/auth');
 const analysisRoutes = require('./routes/analysis');
 
 const app = express();
-
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
